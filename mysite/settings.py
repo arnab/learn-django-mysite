5c0d410e5b38a6ba5261a8ebfe48a6c9721b574a
Arnab Deka <arnab.deka@gmail.com>
2013-07-10 19:19:09 +0530

    setup postgres db

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,11 +11,16 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        # CREATE DATABASE django_tutorial;
+        'NAME': 'django_tutorial',
+
+        # CREATE USER <username> WITH CREATEDB NOCREATEUSER ENCRYPTED PASSWORD '<pwd>';
+        'USER': 'django_dev',
+        'PASSWORD': 'django_dev_pwd',
+
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -29,7 +34,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Calcutta'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
