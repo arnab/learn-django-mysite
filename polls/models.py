ceada28ab4d6f1db519c0ec18d39a0383569d8c6
Arnab Deka <arnab.deka@gmail.com>
2013-07-11 14:40:48 +0530

    Tutorl part 1: a new polls app with a couple of models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,19 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        formatted_date = self.pub_date.strftime("%Y-%m-%d %H:%M %Z")
+        return "{0} [published: {1}]".format(self.question, formatted_date)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __unicode__(self):
+        return "{0} [votes: {1}]".format(self.choice_text, self.votes)
