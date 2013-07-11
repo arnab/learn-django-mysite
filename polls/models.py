e8b44ad2e7210eef4fc4f79f9f1e2a8a9c622739
Arnab Deka <arnab.deka@gmail.com>
2013-07-11 16:13:59 +0530

    a customized polls admin

--- polls/models.py
+++ polls/models.py
@@ -6,14 +6,16 @@ class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __unicode__(self):
-        formatted_date = self.pub_date.strftime("%Y-%m-%d %H:%M %Z")
-        return "{0} [published: {1}]".format(self.question, formatted_date)
+        return self.question
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __unicode__(self):
-        return "{0} [votes: {1}]".format(self.choice_text, self.votes)
+        return self.choice_text
