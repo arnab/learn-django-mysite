fcc7abad184b5463479135e19174982fdab6dab5
Arnab Deka <arnab.deka@gmail.com>
2013-07-12 15:43:02 +0530

    Submit vote and show results

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.http import Http404
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, get_object_or_404
 
-from polls.models import Poll
+from polls.models import Poll, Choice
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
@@ -14,7 +15,18 @@ def detail(request, poll_id):
     return render(request, 'polls/detail.html', { 'poll': poll })
 
 def results(request, poll_id):
-    return HttpResponse("Results for %s" % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', { 'poll': poll })
 
 def vote(request, poll_id):
-    return HttpResponse("Voting on %s" % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'poll': poll, 'error_message': 'Please select a choice',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(poll.id,)))
