7c3bf75f9c34ec3b801517453368d06352c307c5
Arnab Deka <arnab.deka@gmail.com>
2013-07-12 13:20:40 +0530

    A little better views

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("All polls")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = { 'latest_poll_list': latest_poll_list }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("Details for %s" % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', { 'poll': poll })
+
 
 def results(request, poll_id):
     return HttpResponse("Results for %s" % poll_id)
